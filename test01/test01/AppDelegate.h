44db59cbb65bfbf9e3b96b6912d8642c658305e5
nqp <nqp@NQPdeMacBook-Pro.local>
2015-12-08 10:47:41 +0800

    初始化项目

---
+++ test01/test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test01
+//
+//  Created by NQP on 15/12/8.
+//  Copyright © 2015年 NQP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test01/test01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test01
+//
+//  Created by NQP on 15/12/8.
+//  Copyright © 2015年 NQP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
